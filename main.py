89323bc7789002fdec366ab3c593554e96066d40
Ismail H Rana <ismail.hossen@vivacomsolutions.com>
2024-10-18 01:03:51 +0600

    [ADD] init song serializer

---
+++ main.py
@@ -0,0 +1,16 @@
+from song import Song
+from song_serializer import SongSerializer
+
+
+def main():
+    song = Song("1", "Water of Love", "Dire Straits")
+
+    serializer = SongSerializer()
+
+    print(f'JSON: {serializer.serialize(song, "JSON")}')
+    print(f'XML: {serializer.serialize(song, "XML")}')
+    # print(f'YAML: {serializer.serialize(song, "YAML")}')
+
+
+if __name__ == "__main__":
+    main()
