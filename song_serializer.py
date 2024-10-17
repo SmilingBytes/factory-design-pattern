d81c71305c82cf4525e428d58cdfcd471e32afd4
Ismail H Rana <ismail.hossen@vivacomsolutions.com>
2024-10-18 01:20:07 +0600

    [ADD] v2

--- song_serializer.py
+++ song_serializer.py
@@ -5,14 +5,20 @@ import xml.etree.ElementTree as et
 class SongSerializer:
     def serialize(self, song, format):
         if format == "JSON":
-            song_info = {"id": song.song_id, "title": song.title, "artist": song.artist}
-            return json.dumps(song_info)
+            return self._serialize_to_json(song)
         elif format == "XML":
-            song_info = et.Element("song", attrib={"id": song.song_id})
-            title = et.SubElement(song_info, "title")
-            title.text = song.title
-            artist = et.SubElement(song_info, "artist")
-            artist.text = song.artist
-            return et.tostring(song_info, encoding="unicode")
+            return self._serialize_to_xml(song)
         else:
             raise ValueError(format)
+
+    def _serialize_to_json(self, song):
+        payload = {"id": song.song_id, "title": song.title, "artist": song.artist}
+        return json.dumps(payload)
+
+    def _serialize_to_xml(self, song):
+        song_element = et.Element("song", attrib={"id": song.song_id})
+        title = et.SubElement(song_element, "title")
+        title.text = song.title
+        artist = et.SubElement(song_element, "artist")
+        artist.text = song.artist
+        return et.tostring(song_element, encoding="unicode")
